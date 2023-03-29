222b853230a4f8759f1d3ad9acfefd5d5c4e9d01
bert9946 <bert9946@icloud.com>
2023-03-29 13:20:55 +0800

    Updated.

--- AzureOpenai.py
+++ AzureOpenai.py
@@ -1,28 +1,36 @@
 import openai
+import os
 import azure.cognitiveservices.speech as speechsdk
 
-openai.api_key = "Your_api_key"
-speech_config = speechsdk.SpeechConfig(
-    subscription='Your_api_key',
-    region='eastasia')
+# openai.api_key = "Your_api_key"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region='eastasia')
+audio_config = speechsdk.audio.AudioOutputConfig(filename="./voice.wav")
+# zh-TW-HsiaoYuNeural #en-US-AshleyNeural
+# speech_config.speech_synthesis_voice_name = 'zh-TW-HsiaoYuNeural'
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(
+    speech_config=speech_config, audio_config=audio_config)
 
 ans = ""
 messages = [
     {"role": "system", "content": "You are a helpful assistant."},
 ]
-path = './translation/translation.txt'
+path = './transcription.txt'
 f = open(path, 'w', encoding='UTF-8')
-i = 0
-while True:
-    audio_config = speechsdk.audio.AudioOutputConfig(
-        use_default_speaker=True, filename="./voice/file"+str(i)+".wav")
-    # zh-TW-HsiaoYuNeural #en-US-AshleyNeural
-    speech_config.speech_synthesis_voice_name = 'zh-TW-HsiaoYuNeural'
-    speech_synthesizer = speechsdk.SpeechSynthesizer(
-        speech_config=speech_config, audio_config=audio_config)
 
+while True:
+
+    #user input
     messages.append({"role": "user", "content": input("User: ")})
+
+    if "bye" in messages[-1]['content'].lower():
+        print("Bot: Bye")
+        f.close()
+        break
+
+    #openai response
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # gpt-3.5-turbo,text-davinci-003
         messages=messages,
@@ -30,16 +38,16 @@ while True:
         temperature=0.9,
     )
 
-    if "bye" in messages[-1]['content'].lower():
-        print("Bot: Bye")
-        f.close()
-        break
-
-    print("ChatBot: ", response['choices'][0]['message']['content'])
-    print("ChatBot: ", response['choices'][0]['message']['content'], file=f)
-    speech_synthesis_result = speech_synthesizer.speak_text_async(
-        response['choices'][0]['message']['content']).get()
-    i = i+1
+    #extract answer from response
     ans = response['choices'][0]['message']['content']
 
+    #output answer
+    print("ChatBot: ", ans)
+
+    #write answer to file
+    print("ChatBot: ", ans, file=f)
+
+    #add answer to messages
     messages.append({"role": "assistant", "content": ans})
+
+    speech_synthesizer.speak_text_async(ans).get()
