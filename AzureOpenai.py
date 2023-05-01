9bb69dc45ac953c9ee79941723fc1ad110db8f14
LAPTOP-HJFHLI28\kakeru <auchergod@gmail.com>
2023-05-01 13:55:48 +0800

    no message

--- AzureOpenai.py
+++ AzureOpenai.py
@@ -1,53 +1,43 @@
-import openai
 import os
+import openai
 import azure.cognitiveservices.speech as speechsdk
 
 openai.api_type = "azure"
-openai.api_base = os.getenv("OPENAI_API_BASE") 
-openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_version = "2023-03-15-preview"      # The version of the Azure OpenAI resource you created.
+openai.api_base = os.getenv("OPENAI_API_BASE")  # Your Azure OpenAI resource's endpoint value .
+openai.api_key = os.getenv("OPENAI_API_KEY")    # Your Azure OpenAI resource's key value.
 
-speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region='AZURE_SPEECH_REGION')
-audio_config = speechsdk.audio.AudioOutputConfig(filename="./voice.wav")
-# zh-TW-HsiaoYuNeural #en-US-AshleyNeural
-# speech_config.speech_synthesis_voice_name = 'zh-TW-HsiaoYuNeural'
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region='eastasia')
+speech_config.speech_synthesis_language = "en-US"
+speech_config.speech_synthesis_voice_name = 'zh-TW-HsiaoYuNeural'
+
+audio_config = speechsdk.audio.AudioOutputConfig(filename="./voice/voice.wav")
 
 speech_synthesizer = speechsdk.SpeechSynthesizer(
-    speech_config=speech_config, audio_config=audio_config)
+    speech_config=speech_config, 
+    audio_config=audio_config)
 
-ans = ""
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-]
-path = './transcription.txt'
+path = './translation/transcription.txt'
 f = open(path, 'w', encoding='UTF-8')
 
-while True:
+conversation=[{"role": "system", "content": "You are a helpful assistant."}]
 
-    #user input
-    messages.append({"role": "user", "content": input("User: ")})
+while(True):    
+    conversation.append({"role": "user", "content": input("User: ") + "Please use that to create a story, rephrase it as one sentence."})
 
-    if "bye" in messages[-1]['content'].lower():
-        print("Bot: Bye")
+    if "bye" in conversation[-1]['content'].lower():
+        print("Chatbot: Bye")
         f.close()
         break
-
-    #openai response
+    
     response = openai.ChatCompletion.create(
-        model="gpt-35-turbo",  # gpt-3.5-turbo,Azure setname
-        messages=messages,
+        engine="gpt-35-turbo", # The deployment name you chose when you deployed the ChatGPT or GPT-4 model.
+        messages = conversation
     )
 
-    #extract answer from response
     ans = response['choices'][0]['message']['content']
-
-    #output answer
-    print("ChatBot: ", ans)
-
-    #write answer to file
-    print("ChatBot: ", ans, file=f)
-
-    #add answer to messages
-    messages.append({"role": "assistant", "content": ans})
-
+    conversation.append({"role": "assistant", "content": ans})
+    print("Chatbot: ", ans, file=f)
+    print("Chatbot: ", ans)
+    
     speech_synthesizer.speak_text_async(ans).get()
