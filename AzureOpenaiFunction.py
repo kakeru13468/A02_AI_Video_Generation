ffcf5c57425e55bdbb36738bd7f1649b5180449d
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-05-02 23:11:41 +0800

    format, add TODO, and edit prompt

--- AzureOpenaiFunction.py
+++ AzureOpenaiFunction.py
@@ -5,23 +5,25 @@ import azure.cognitiveservices.speech as speechsdk
 class create():
     def __init__(self):
         pass
-    
+
     def Text(text):
+        #TODO add querying prompt for video generation
+
         openai.api_type = "azure"
         openai.api_base = os.getenv("OPENAI_API_BASE")
         openai.api_version = "2023-03-15-preview"
         openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
         path = './transcription.txt'
         f = open(path, 'w', encoding='UTF-8')
-    
+
         conversation=[{"role": "system", "content": "You are a director and screenwriter. You writes video scripts and prompt for video generation."}]
-        conversation.append({"role": "user", "content": text + ",Create a story using this text with a length of no more than 10 words."})
-        
+        conversation.append({"role": "user", "content": text + ", create a story using this text."})
+
         response = openai.ChatCompletion.create(
             engine="gpt-35-turbo", # The deployment name you chose when you deployed the ChatGPT or GPT-4 model.
             messages = conversation
-        )   
-    
+        )
+
         ans = response['choices'][0]['message']['content']
         #conversation.append({"role": "assistant", "content": ans})
         print(ans, file=f)
@@ -37,9 +39,7 @@ class create():
         speech_synthesizer = speechsdk.SpeechSynthesizer(
             speech_config=speech_config, 
             audio_config=audio_config)
-    
-        speech_synthesizer.speak_text_async(voice).get()
-    
-        return("success")
 
-    
+        speech_synthesizer.speak_text_async(voice).get()
+
+        return("success")
