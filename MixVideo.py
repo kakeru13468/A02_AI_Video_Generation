0e07fd540289604aa78fccace1c2d4b9588d41c9
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-05-17 17:52:42 +0800

    formatted

--- MixVideo.py
+++ MixVideo.py
@@ -4,8 +4,8 @@ def merge_video_audio(video_input, audio_input, output_file):
     #找需要合併之檔案位置
     video = VideoFileClip(video_input)
     audio = AudioFileClip(audio_input)
-    audio.write_audiofile('./voice/audio.mp3') 
-    
+    audio.write_audiofile('./voice/audio.mp3')
+
     final_video = video.set_audio(audio)
 
     #libx264是一種影像編碼格式，aac是一種音訊編碼格式
