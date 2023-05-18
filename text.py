1e3609f958c82f0d161dfdd33e157bbbda42eab5
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-05-18 16:13:11 +0800

    Updated text function

--- text.py
+++ text.py
@@ -10,26 +10,23 @@ def Text(input):
 	openai.api_version = "2023-05-15"
 	openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
 
-	# conversation=[{"role": "system", "content": "You are a director and screenwriter. You writes video scripts and prompt for video generation."}]
-	# conversation.append({"role": "user", "content": input + ", create a story using this text."})
-
+	#read prompt from file
 	with open('prompt.json', 'r') as f:
 		conversation = json.load(f)
+
+	conversation.append({"role": "user", "content": input})
 
 	response = openai.ChatCompletion.create(
 		engine="gpt-35-turbo", # The deployment name you chose when you deployed the ChatGPT or GPT-4 model.
 		messages = conversation
 	)
-	print('sending message to OpenAI')
 
 
 	ans = response['choices'][0]['message']['content']
 	#conversation.append({"role": "assistant", "content": ans})
 
 	# write to file
-	path = './script.txt'
-	f = open(path, 'w', encoding='UTF-8')
-	print(ans, file=f)
-	f.close()
+	with open('./script.txt', 'w', encoding='UTF-8') as f:
+		print(ans, file=f)
 
 	return ans
