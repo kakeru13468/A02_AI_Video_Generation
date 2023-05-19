fe2053a58f686fcee44e37311e212d8d52ce9d47
bert9946 <bert9946@icloud.com>
2023-05-19 14:16:40 +0800

    added Accelerate

--- text2video.py
+++ text2video.py
@@ -2,12 +2,16 @@ import torch
 import imageio
 from diffusers import TextToVideoZeroPipeline
 import tomesd
+from accelerate import Accelerator
+
+accelerator = Accelerator()
+device = accelerator.device
 
 def text2video(prompt):
 	model_id = "runwayml/stable-diffusion-v1-5"
-	pipe = TextToVideoZeroPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
+	pipe = TextToVideoZeroPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to(device)
 	tomesd.apply_patch(pipe, ratio=0.5)
 
 	result = pipe(prompt=prompt).images
 	result = [(r * 255).astype("uint8") for r in result]
-	imageio.mimsave("video.mp4", result, fps=2)
+	imageio.mimsave("video.mp4", result, fps=4)
