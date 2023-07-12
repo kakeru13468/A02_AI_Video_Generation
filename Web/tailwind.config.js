441854d2ba26961189c8b3cd9e69bf4ae6d163c4
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-12 14:52:11 +0800

    setup Tailwind

--- Web/tailwind.config.js
+++ Web/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
