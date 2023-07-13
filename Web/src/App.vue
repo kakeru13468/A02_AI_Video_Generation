0c3afb8d297003d0ea9a9aadd2f3f53e85e4b603
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-13 15:37:10 +0800

    add layout

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mx-4 pt-16 lg:mx-32">
+    <div class="grid grid-cols-12 max-h-screen grid-flow-row auto-cols-max gap-4">
+      <textarea class="h-8 border col-span-10 max-h-24 resize-none"></textarea>
+      <button type="button" class="border rounded-sm col-span-2">generate</button>
+      <div class="p-2 col-span-3 h-full bg-indigo-400"></div>
+      <video src="../../media/combined.mp4" class="w-full col-span-9 aspect-video bg-black" controls></video>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
