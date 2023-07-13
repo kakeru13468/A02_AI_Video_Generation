27ab6a29c0659e330ad5f7f88b70c6e0a05dbc9d
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-13 21:13:49 +0800

    add Mac support

--- text2video.py
+++ text2video.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import imageio
 from diffusers import TextToVideoZeroPipeline
@@ -7,11 +8,18 @@ from accelerate import Accelerator
 accelerator = Accelerator()
 device = accelerator.device
 
-def text2video(prompt):
+def text2video(prompt, fps=4):
 	model_id = "runwayml/stable-diffusion-v1-5"
-	pipe = TextToVideoZeroPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to(device)
-	tomesd.apply_patch(pipe, ratio=0.5)
+	
+	#if on mac
+	if sys.platform == "darwin":
+		# device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+		pipe = TextToVideoZeroPipeline.from_pretrained(model_id).to(device)
+		pipe.enable_attention_slicing() #for low memory usage
+	else:
+		pipe = TextToVideoZeroPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to(device)
+		tomesd.apply_patch(pipe, ratio=0.5)
 
 	result = pipe(prompt=prompt).images
 	result = [(r * 255).astype("uint8") for r in result]
-	imageio.mimsave("video.mp4", result, fps=4)
+	imageio.mimsave("video.mp4", result, fps=fps)
