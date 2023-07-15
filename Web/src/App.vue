5d98055e4a7c104fab2677c600425790b2184a81
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-15 15:48:57 +0800

    minor fix

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="mx-4 pt-16 lg:mx-32">
-    <div class="grid grid-cols-12 max-h-screen grid-flow-row auto-cols-max gap-4">
-      <textarea class="h-8 border col-span-10 max-h-24 resize-none area-lg"  placeholder="prompt"></textarea>
-      <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full ">generate</button>
+  <div class="gap-4 mx-4 pt-16 lg:mx-32">
+    <div class="grid grid-cols-12 max-h-screen grid-flow-row auto-cols-max gap-1 lg:gap-2">
+      <textarea class="h-8 border col-span-8 md:col-span-10 max-h-24 resize-none area-lg"  placeholder="prompt"></textarea>
+      <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 col-span-4 md:col-span-2 rounded-full ">generate</button>
       <!--<div class="p-2 col-span-3 h-full bg-indigo-400"></div>-->
       <div class="col-span-3 h-full bg-indigo-400 max-w-sm rounded overflow-hidden shadow-lg"></div>
-      <video class="w-full col-span-8 aspect-video bg-black" controls></video>
+      <video class="w-full col-span-9 rounded aspect-video bg-black" controls></video>
     </div>
   </div>
 </template>
