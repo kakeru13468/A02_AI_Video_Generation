c8c446dc85332611a8e9c8801fc0b79bff368c1b
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-15 17:54:45 +0800

    remove redundant

--- MixVideo.py
+++ MixVideo.py
@@ -4,7 +4,6 @@ def merge_video_audio(video_input, audio_input, output_file):
     #找需要合併之檔案位置
     video = VideoFileClip(video_input)
     audio = AudioFileClip(audio_input)
-    audio.write_audiofile('./voice/audio.mp3')
 
     final_video = video.set_audio(audio)
 
