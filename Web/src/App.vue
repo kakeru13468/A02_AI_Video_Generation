9bf2d29445db6348337682a23d6100870b2e43ed
LAPTOP-HJFHLI28\kakeru <auchergod@gmail.com>
2023-07-16 12:39:49 +0800

    add video message and Download button

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -5,7 +5,16 @@
       <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 col-span-4 md:col-span-2 rounded-full ">generate</button>
       <!--<div class="p-2 col-span-3 h-full bg-indigo-400"></div>-->
       <div class="md:col-span-3 col-span-full w-full p-4 h-full bg-indigo-400 rounded overflow-hidden shadow-lg"></div>
-      <video class="w-full col-span-full md:col-span-9 rounded aspect-video bg-black" controls></video>
+      <div class="md:col-span-9">
+        <video class="w-full col-span-full rounded aspect-video bg-black" controls></video>
+        <div class="mt-4">
+          <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Video name" aria-label="Full name" disabled="disabled">
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+            <span>Download</span>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
