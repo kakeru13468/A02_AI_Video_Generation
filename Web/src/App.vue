b42fd767ba16773bf46015a068165ba219d7cd89
LAPTOP-HJFHLI28\kakeru <auchergod@gmail.com>
2023-07-16 12:57:19 +0800

    change video name and download button location

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -7,10 +7,12 @@
       <div class="md:col-span-3 col-span-full w-full p-4 h-full bg-indigo-400 rounded overflow-hidden shadow-lg"></div>
       <div class="md:col-span-9">
         <video class="w-full col-span-full rounded aspect-video bg-black" controls></video>
-        <div class="mt-4">
-          <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Video name" aria-label="Full name" disabled="disabled">
+        <div class="mt-4 flex">
+          <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Show Video name" aria-label="Full name" disabled="disabled">
           <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+            </svg>
             <span>Download</span>
           </button>
         </div>
