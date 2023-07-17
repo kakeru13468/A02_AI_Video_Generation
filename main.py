adb8dd53ccdebfa87763d991c9c83c22ae37588d
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-17 12:27:50 +0800

    add option to dry run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import argparse, sys
 
 import mixVideo
 import text
@@ -14,13 +14,35 @@ def getLine(text, type, line):
 	return paragraph
 
 
-if __name__ == '__main__':
-	input = sys.argv[1]
-	text = text.Text(input)
-	script = getLine(text, "Scripts", 0)
-	prompt = getLine(text, "Prompts", 0)
-	
-	voice = voice.Voice(script)
-	video = text2video.text2video(prompt, fps=2,dry_run=False)
+def main():
+	parser=argparse.ArgumentParser()
 
-	mixVideo.merge_video_audio(video, voice, "./media/output.mp4")
+	dry_run = False
+
+	parser.add_argument("--dry-run", help="use example video", action="store_true")
+	parser.add_argument("prompt", help="prompt to generate video from",nargs='+')
+	args = parser.parse_args()
+
+	if args.dry_run: dry_run = True
+
+	#combine args.prompt into one string
+	args.prompt = ' '.join(args.prompt)
+
+	input = args.prompt
+
+	print("Generating script.")
+	generated_text = text.Text(input)
+	script = getLine(generated_text, "Scripts", 0)
+	prompt = getLine(generated_text, "Prompts", 0)
+
+	generated_voice = voice.Voice(script)
+	video = text2video.text2video(prompt, fps=4, dry_run=dry_run)
+
+	videoName =  str(hash(script + prompt))
+
+	outputPath = "./media/" + videoName + ".mp4"
+	mixVideo.merge_video_audio(video, generated_voice, outputPath)
+	print(outputPath)
+
+if __name__ == '__main__':
+	main()
