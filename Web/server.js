35edda18a6c1b1337e14146dd35be31418e9c969
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-07-29 16:35:21 +0800

    change to return example.mp4 if dry_run

--- Web/server.js
+++ Web/server.js
@@ -9,32 +9,36 @@ app.use(express.static(__dirname));
 const absolutePath = path.join(__dirname, '../')
 
 app.post('/generate', (req, res) => {
-	if (res);
+	let videoPath = '';
 
 	var prompt = req.query.prompt;
-	if (req.query.dry_run) var dry_run = true;
+	if (req.query.dry_run) {
+		var dry_run = true;
+		console.log('##### dry run #####');
+		videoPath = 'example.mp4';
+	}
+	else {
+		let args = [path.join(absolutePath, 'main.py'), prompt, dry_run? '--dry-run' : ''];
 
+		let child_process = spawn('python', args, {cwd: absolutePath});
+		console.log('##### start generating #####');
+		
+		child_process.stdout.on('data', (data) => {
+			console.log(`stdout: ${data}`);
+			if (String(data).match(/\.\/media\/[\d]*\.mp4/)) {
+				videoPath = String(data).match(/\.\/media\/[\d]*\.mp4/)[0];
+			}
+		});
+		child_process.stderr.on('data', (data) => {
+			console.log(`stderr: ${data}`);
+		});
+		child_process.on('close', (code) => {
+			console.log(`child process exited with code ${code}`);
+		});
+	}
 
-	let args = [path.join(absolutePath, 'main.py'), prompt, dry_run? '--dry-run' : ''];
-
-	let child_process = spawn('python', args, {cwd: absolutePath});
-	let videoPath = '';
-	
-	child_process.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`);
-		if (String(data).match(/\.\/media\/[\d]*\.mp4/)) {
-			videoPath = String(data).match(/\.\/media\/[\d]*\.mp4/)[0];
-		}
-	});
-	child_process.stderr.on('data', (data) => {
-		console.log(`stderr: ${data}`);
-	});
-	child_process.on('close', (code) => {
-		console.log(`child process exited with code ${code}`);
-
-		res.send(videoPath);
-	});
-
+	console.log('video: '+videoPath);
+	res.send(videoPath);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}.`))
