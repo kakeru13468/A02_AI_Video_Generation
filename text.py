70eb1e34c21c78b9d80462b9cd32df95b6efc897
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-08-02 17:48:00 +0800

    adjust the prompts

--- text.py
+++ text.py
@@ -12,7 +12,7 @@ def Text(input):
 
 	conversation = [
 	{"role": "system", "content": "You are a director and screenwriter. You writes video scripts and prompt for video generation."},
-	{"role": "user", "content": "based on the text provided, Generate a set of: 1. scripts, and 2. prompts for image/video generation of several scene that can be combined into a interesting story, each prompt wil be describing the coresponding scene."},
+	{"role": "user", "content": "based on the text provided, Generate a set of: 1. scripts, and 2. prompts for image/video generation of several scene that can be combined into a interesting story, each prompt will be describing the corresponding scene."},
 	{"role": "user", "content": "A dog running in the beach."},
 	{"role": "assistant", "content": '''Scripts:
 1. A black Labrador runs out from a house towards the beach.
@@ -23,12 +23,12 @@ def Text(input):
 6. As the sun sets and the sky turns orange, the dog lays down next to the children, panting happily.
 
 Prompts:
-1. Capture the dog's excitement as he leaves the house and runs towards the beach.
-2. Highlight the beauty of the ocean as the dog runs along the shoreline.
-3. Get low to the ground to capture the dog splashing through the water.
-4. Film the children's reactions to the dog as they play with him.
-5. Capture the dog's playful spirit as he runs with the children and jumps through the waves.
-6. Show the beauty of the sunset as the dog and the children lay on the beach together.'''}
+1. A black Labrador runs out from a house towards the beach.
+2. A black Labrador runs along the shoreline of the beautiful ocean.
+3. A black Labrador jumps into the water and starts swimming playfully.
+4. A group of children playing on the beach with the black Labrador.
+5. A black Labrador runs with the children along the beach, playing fetch and leaping through the waves.
+6. A black Labrador and the children lay on the beach together as the sun sets.'''}
 ]
 
 	conversation.append({"role": "user", "content": input})
