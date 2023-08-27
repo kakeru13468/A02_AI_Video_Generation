0fa8c6e7b772e9db0b58e4b39bb074553f701117
bert9946 <53603989+bert9946@users.noreply.github.com>
2023-08-27 11:26:58 +0800

    add option for GPT-4

--- text.py
+++ text.py
@@ -2,15 +2,20 @@ import openai
 import os
 import json
 
-def Text(input):
+def Text(input, engine="gpt-35-turbo"):
 
 	openai.api_type = "azure"
-	openai.api_base = os.getenv("OPENAI_API_BASE")
 	openai.api_version = "2023-05-15"
-	openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+
+	if engine == "gpt-35-turbo":
+		openai.api_base = os.getenv("OPENAI_API_BASE")
+		openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+	elif engine == "kakeruGPT4":
+		openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT_GPT4")
+		openai.api_key = os.getenv("AZURE_OPENAI_KEY_GPT4")
 
 	conversation = [
-	{"role": "system", "content": "You are a director and screenwriter. You writes video scripts and prompt for video generation."},
+	{"role": "system", "content": "You are a director and prompt engineer. You write video scripts and prompt for video generation."},
 	{"role": "user", "content": "based on the text provided, Generate a set of: 1. scripts, and 2. prompts for image/video generation of several scene that can be combined into a interesting story, each prompt will be describing the corresponding scene."},
 	{"role": "user", "content": "A dog running in the beach."},
 	{"role": "assistant", "content": '''{"Scripts": ["A black Labrador runs out from a house towards the beach.",
@@ -30,7 +35,7 @@ def Text(input):
 	conversation.append({"role": "user", "content": input})
 
 	response = openai.ChatCompletion.create(
-		engine="gpt-35-turbo", # The deployment name you chose when you deployed the ChatGPT or GPT-4 model.
+		engine=engine,
 		messages = conversation
 	)
 
