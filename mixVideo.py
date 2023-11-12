35a1013a7d58d9b387f1db5823499e2330706813
bert9946 <bert9946@icloud.com>
2023-11-12 14:36:24 +0800

    add option to loop the clip

--- mixVideo.py
+++ mixVideo.py
@@ -1,9 +1,12 @@
 from moviepy.editor import *
 
-def merge_video_audio(video_input, audio_input, output_file):
+def merge_video_audio(video_input, audio_input, output_file, loop=False):
     #找需要合併之檔案位置
     video = VideoFileClip(video_input)
     audio = AudioFileClip(audio_input)
+
+    if loop:
+        video = video.loop(duration=audio.duration)
 
     final_video = video.set_audio(audio)
 
